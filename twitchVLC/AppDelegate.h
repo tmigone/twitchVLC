0cee536d2b1044d930a8d3169659386b7b10ed24
tomasmigone <tomasmigone@gmail.com>
2013-10-17 01:23:59 -0300

    Added twitchLauncher class (working).

--- twitchVLC/AppDelegate.h
+++ twitchVLC/AppDelegate.h
@@ -12,4 +12,21 @@
 
 @property (assign) IBOutlet NSWindow *window;
 
+-(IBAction)buttonPress:(id)sender;
+
+@end
+
+
+@interface twitchLauncher : NSObject
+{
+    NSString *channelName;
+    NSString *token;
+    NSString *signature;
+}
+
+-(void)defaultValues;
+-(void)getJSON;
+-(void)getM3U8;
+-(void)parseM3U8;
+-(void)launchVLC;
 @end
--- twitchVLC/AppDelegate.m
+++ twitchVLC/AppDelegate.m
@@ -15,4 +15,70 @@
     // Insert code here to initialize your application
 }
 
+-(IBAction)buttonPress:(id)sender
+{
+    twitchLauncher *launcher = [[twitchLauncher alloc] init];
+    [launcher defaultValues];
+    [launcher getJSON];
+    [launcher getM3U8];
+    [launcher parseM3U8];
+    //[launcher launchVLC];
+    
+}
+
+@end
+
+@implementation twitchLauncher
+-(void)defaultValues
+{
+    channelName = @"idrajit";
+}
+
+-(void)getJSON
+{
+    NSString *urlString = [NSString stringWithFormat:@"%@%@%@", @"http://api.twitch.tv/api/channels/", channelName, @"/access_token"];
+    NSURL *url = [NSURL URLWithString: urlString];
+    
+    NSData *jsonData = [NSData dataWithContentsOfURL:url];
+    
+    if(jsonData != nil)
+    {
+        NSError *error = nil;
+        NSDictionary *result = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers error:&error];
+        
+        if (error == nil)
+        {
+            token = result[@"token"];
+            signature = result[@"sig"];
+        }
+    }
+}
+
+-(void)getM3U8
+{
+    NSString *urlString = [NSString stringWithFormat:@"%@%@%@%@%@%@", @"http://usher.twitch.tv/api/channel/hls/", channelName, @".m3u8?token=", token, @"&sig=", signature];
+    
+    NSURL *url = [NSURL URLWithString:[urlString stringByAddingPercentEscapesUsingEncoding: NSUTF8StringEncoding]];
+    
+    
+    NSData *pageContents = [NSData dataWithContentsOfURL:url];
+    
+    NSError *error = nil;
+    NSString *path=@"/Users/tomi/Desktop/test.m3u8";
+    [pageContents writeToFile:path options:NSDataWritingAtomic error:&error];
+    
+}
+
+-(void)parseM3U8
+{
+    NSString * fileContents = [NSString stringWithContentsOfFile:@"/Users/tomi/Desktop/test.m3u8" encoding:NSUTF8StringEncoding error:nil];
+    
+    //id variable = [M3U8Parser m3u8SegmentInfoListFromPlanString:fileContents baseURL:nil];
+    
+}
+-(void)launchVLC
+{
+    [[NSWorkspace sharedWorkspace] openFile:@"/Users/tomi/Desktop/test.m3u8" withApplication:@"VLC"];
+}
+
 @end
